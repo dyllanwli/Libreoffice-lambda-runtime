70c1363eb596978fb23192d1702c3f0e7c2a878b
DY-L <dyllanwli@users.noreply.github.com>
2019-05-08 12:24:18 +0800

    Create bootstrap.sh

---
+++ lo/bootstrap.sh
@@ -0,0 +1,15 @@
+# !/bin/bash
+
+export LO_VERSION='6.2.3'
+yum update -y && yum install -y \
+    java-1.8.0-openjdk-devel \
+    libcairo.so.2 \
+    tar
+
+curl -sL http://download.documentfoundation.org/libreoffice/stable/${LO_VERSION}/rpm/x86_64/LibreOffice_${LO_VERSION}_Linux_x86-64_rpm.tar.gz | tar -xz
+mv LibreOffice* LibreOffice && cd LibreOffice && yum localinstall -y RPMS/*rpm
+
+
+# echo "hello world" > a.txt
+# /opt/libreoffice6.2/program/soffice --headless --invisible --nodefault --nofirststartwizard \
+#     --nolockcheck --nologo --norestore --convert-to pdf --outdir $(pwd) a.txt
